b84f758aa685733511421524afe3768cf8b9b3bd
harsha2006-10 <hvardhann03@gmail.com>
2025-04-12 19:26:44 +0530

    about student marks

--- new.c
+++ new.c
@@ -6,4 +6,13 @@ struct student
     char name[20];
     float marks;
 };
+char calcu(float marks)
+{
+	if(marks>=90) return'A';
+    else if(marks>=80) return'B';
+    else if(marks>=70) return'c';
+    else if(marks>=60) return'D';
+    else if(marks>=50) return'E';
+    else return'F'
+}
 
